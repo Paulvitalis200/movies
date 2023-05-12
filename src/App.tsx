0ea7d724309a0cfa040da3851a4e600ec0bdabf8
Paul Otieno <vitalispaul48@live.com>
2023-05-12 07:20:36 +0300

    Add css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
+import "./App.css";
 
 function App() {
+  let items = ["Nairobi", "Kenya"];
   return (
     <div>
-      <ListGroup />
+      <ListGroup
+        items={items}
+        heading="List"
+        onSelectItem={() => {}}
+      ></ListGroup>
     </div>
   );
 }
